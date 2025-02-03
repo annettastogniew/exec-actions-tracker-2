a545f4d31e01d799d3f971f4758fc5acddeb4ae6
Stogniew <stogniew.a@northeastern.edu>
2025-02-03 14:05:06 -0500

    fade for scroll

--- src/lib/Table.svelte
+++ src/lib/Table.svelte
@@ -1,33 +1,42 @@
 <script>
     import ExpandableRow from "./ExpandableRow.svelte";
-    import { allActions, filter } from './state.svelte';
+    import { allActions, filter } from "./state.svelte";
     import { ExclamationTriangle } from "svelte-bootstrap-icons";
 
-    const filterData = action => {
-        const filters = Object.keys(filter).filter(col => col !== "Date");
-        const categoryAndPrimaryTag = filters.map(col => filter[col].includes(action[col])).every(Boolean);
+    const filterData = (action) => {
+        const filters = Object.keys(filter).filter((col) => col !== "Date");
+        const categoryAndPrimaryTag = filters
+            .map((col) => filter[col].includes(action[col]))
+            .every(Boolean);
         const date = filter["Date"] <= new Date(action["Date"]).getTime();
         return categoryAndPrimaryTag && date;
-    }
+    };
 
-    const selectedActions = $derived(allActions["data"].filter(action => filterData(action)));
+    const selectedActions = $derived(
+        allActions["data"].filter((action) => filterData(action)),
+    );
     let currentCount = $derived(selectedActions.length),
         totalCount = allActions["data"].length;
-
 </script>
 
 <main>
     {#if selectedActions.length > 0}
-    <p class="table-msg">Showing {currentCount} out of {totalCount} total actions. Actions marked with <ExclamationTriangle height={14} width={14}/> have been legally challenged or blocked.</p>
+        <p class="table-msg">
+            Showing {currentCount} out of {totalCount} total actions. Actions marked
+            with <ExclamationTriangle height={14} width={14} /> have been legally
+            challenged or blocked.
+        </p>
         <table style:border-spacing={"0"}>
             <tbody>
                 {#each selectedActions as action}
-                    <ExpandableRow rowInfo={action}/>
+                    <ExpandableRow rowInfo={action} />
                 {/each}
             </tbody>
         </table>
     {:else}
-        <p class="none-msg"><em>No executive actions match your specifications.</em></p>
+        <p class="none-msg">
+            <em>No executive actions match your specifications.</em>
+        </p>
     {/if}
 </main>
 
@@ -35,18 +44,24 @@
     main {
         max-height: 300px !important;
         overflow-y: scroll;
+        mask-image: linear-gradient(
+            to bottom,
+            black calc(100% - 48px),
+            transparent 100%
+        );
     }
-    
+
     table {
         width: 98vw !important;
         margin: auto;
+        margin-bottom: 40px;
     }
 
     .none-msg {
         color: #484848;
         margin-bottom: 10px;
     }
-    
+
     .table-msg {
         color: #484848;
         font-size: 14px;
